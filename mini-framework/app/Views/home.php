ac44e51988dffb99cd99f8ac68c86cd56c6f5513
Daryl786 <168486268+Daryl786@users.noreply.github.com>
2025-06-18 18:58:47 -0300

    Add files via upload
    creamos el boton registrarse,el formulario del mismo y podemos ver los clientes

--- mini-framework/app/Views/home.php
+++ mini-framework/app/Views/home.php
@@ -1,9 +1,12 @@
-    <h1><?= $title ?></h1>
+   <img src="/img/logo.png" alt="logo proyecto 3333" width="200">
+ 
+   <h1><?= $title ?></h1>
     <p>Este es un framework educativo para aprender los conceptos fundamentales de PHP MVC.</p>
     
     <?php if ($auth['check']): ?>
         <p>Bienvenido <?= htmlspecialchars($auth['user']['username']) ?></p>
     <?php else: ?>
-        <p>Por favor <a href="/login">inicia sesión</a> para acceder al dashboard.</p>
+	<p>Por favor <a href="/login">inicia sesión</a> para acceder al dashboard.</p>
+	<a href="/register">regístrate aquí</a> para acceder al dashboard.
     <?php endif; ?>
 
